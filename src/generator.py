ee1fd305b1f3e41dc38a4f52511bc24bfe1dd4d7
DB <db@mac.local>
2026-02-09 20:14:55 +0530

    added generator

--- src/generator.py
+++ src/generator.py
@@ -3,9 +3,23 @@ Contains the class responsible for building things
 """
 import os
 import shutil
+from pathlib import Path
 from tempfile import TemporaryDirectory
 
 from src.extensions.livepreview import LivePreview
+
+def copy_dir_contents(src, dst):
+    src = Path(src)
+    dst = Path(dst)
+
+    dst.mkdir(parents=True, exist_ok=True)
+
+    for item in src.iterdir():
+        target = dst / item.name
+        if item.is_dir():
+            shutil.copytree(item, target, dirs_exist_ok=True)
+        else:
+            shutil.copy2(item, target)  # preserves metadata
 
 class Generator:
     def __init__(self, ext, shellcode, enc):
@@ -25,13 +39,29 @@ class Generator:
         """template files for compilation"""
 
         # Copy shellcode file
-        temp_shellcode = os.path.join(self.tmp, "payload.bin")
+        temp_shellcode = os.path.join(self.tmp, "../payload.bin")
         shutil.copyfile(self.shellcode, temp_shellcode)
         print("[+] Shellcode copied to:\t\t"+temp_shellcode) 
 
-        # First copy files into temp directory 
-
-
+        # First copy vs files into temp directory 
+        copy_dir_contents(self.ext.template_path, self.tmp)
+        print("[+] Template JS copied to:\t\t" + self.tmp) 
 
         # Copy rust files 
+        rust_files = ["Cargo.toml", "Cargo.lock", os.path.join("src", "lib.rs")]
+        for r in rust_files:
+            _s = os.path.join(os.getcwd(), "templates", "rust", r)
+            _d = os.path.join(self.tmp, r)
+            shutil.copy2(_s, _d)
+            print("[+] Copied rust files to:\t\t" + _d) 
+
+    def compile(self):
+        """Compile files"""
+        cw = os.getcwd()
+        try:
+            os.chdir(self.tmp)
+
+        except Exception as e:
+            pass
+        os.chdir(cw)
     
